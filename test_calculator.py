c897e497b9bcb99c8654560e9699f91fea1c16ec
MOOMOO888 <s6552410003@sau.ac.th>
2024-11-05 13:51:36 +0700

    test

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+# tests/test_calculator.py
+
+import unittest
+from app.calculator import add, subtract
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(3, 2), 5)
+        self.assertEqual(add(-1, 1), 0)
+    
+    def test_subtract(self):
+        self.assertEqual(subtract(3, 2), 1)
+        self.assertEqual(subtract(-1, 1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
